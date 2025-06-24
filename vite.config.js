b78f1a996a463ea8b4dc6d7b27a931a664a51770
Ibne Arif Al Riham <riham.tangail@gmail.com>
2025-06-24 19:34:15 +0600

    asyuh

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Read env variable or default to root
+const base = process.env.VERCEL ? '/' : '/portfolio/'
+
 export default defineConfig({
-  base: '/portfolio/', // ✅ Correct: absolute path
+  base,
   plugins: [react()],
 })
